6a0a8807fc9a48f31b8685c9516cd244b7893edf
sumitradas94 <sumitradas94@gmail.com>
2022-12-24 10:37:16 +0530

    Added harmonic file

---
+++ TwoPower.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class TwoPower
+    {
+        public static void power()
+        {
+            Console.WriteLine("=====================");
+            Console.WriteLine("Enter a value");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if ((n >= 0) && (n < 31))
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    double result = Math.Pow(2, i);
+                    Console.WriteLine(result);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter valid n value");
+            }
+        }
+        
+
+    }
+}
