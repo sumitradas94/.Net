8e2148a7c01e16849ac059c551f09459e259029f
sumitradas94 <sumitradas94@gmail.com>
2022-12-24 10:29:28 +0530

    This is Harmonic program

---
+++ Harmonic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Harmonic
+    {
+        public static void Har()
+        {
+            Console.WriteLine("Give the nth harminic value");
+            int h = Convert.ToInt32(Console.ReadLine());
+            {
+                double result = 0.0;
+                if (h != 0)
+                {
+                    for (int i = 1; i <= h; i++)
+                    {
+
+                        result += 1 / (float)i;
+
+
+                    }
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("enter a valid number");
+
+                }
+            }
+        }
+    }
+}
+
